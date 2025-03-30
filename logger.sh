06056a22f4eb985de30aa31db4e9bc55fc0f87f6
saulotarsobc <saulotarsobc@gmail.com>
2025-03-30 19:48:46 -0300

    Enhance logger output format for better readability

--- logger.sh
+++ logger.sh
@@ -1,4 +1,6 @@
-echo "ANDROID_HOME=$ANDROID_HOME" && \
-echo "ANDROID_SDK_ROOT=$ANDROID_SDK_ROOT" && \
-echo "ANDROID_NDK_HOME=$ANDROID_NDK_HOME" && \
-echo "PATH=$PATH"
+echo "\n===================================";
+echo "ANDROID_HOME=$ANDROID_HOME";
+echo "ANDROID_SDK_ROOT=$ANDROID_SDK_ROOT";
+echo "ANDROID_NDK_HOME=$ANDROID_NDK_HOME";
+echo "PATH=$PATH";
+echo "===================================\n";
